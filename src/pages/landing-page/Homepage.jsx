c950f5cd3eb3eb59e473ab3aebb0897d7360051f
Segun Osiki <osiki.segun@gmail.com>
2023-02-06 19:09:50 +0100

    add paragraph to section

--- src/pages/landing-page/Homepage.jsx
+++ src/pages/landing-page/Homepage.jsx
@@ -15,7 +15,18 @@ const Homepage = () => {
                 <section className=" md:flex">
                     <div className="md:flex-1">
                         <div className="non-italic">
-                            <h1 className="font-normal text-2xl leading-7 ml-6 md:font-bold md:text-5xl md:leading-[3.813rem] md:ml-24 text-[#3A0CA3] md:w-[90%] md:py-16 md:px-4">Quick and easy payment platform for all your transactions</h1>
+                            <h1 className="font-normal text-2xl leading-7 ml-6 mb-4 md:text-3xl
+                            lg:font-bold lg:text-5xl lg:leading-[3.813rem] lg:ml-24 
+                            text-[#3A0CA3] lg:w-[90%] lg:pt-16 lg:pl-4"
+                            >
+                                Quick and easy payment platform for all your transactions
+                            </h1>
+
+                            <p className="ml-6 
+                            lg:w-[70%] lg:font-normal lg:text-base lg:leading-6 text-[#012A4A] lg:ml-28">
+                                Save and manage all your transaction in one place, easy payment 
+                                anytime & anyday.
+                            </p>
                         </div>
                     </div>
                     <div className="md:flex-2 md:bg-red-700">middle</div>
